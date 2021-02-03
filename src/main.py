9bb827564b406a2611138063c3d5738246c1fc8b
isanchez <ivan.sanchezc@opendeusto.es>
2021-02-03 21:24:31 +0100

    fixed?

--- src/main.py
+++ src/main.py
@@ -3,18 +3,18 @@ from grove.grove_ultrasonic_ranger import GroveUltrasonicRanger
 import seeed_dht
 
 def readTemperatureAndHumidity(): 
-    sensor_dht11 = seeed_dht.DHT("11", 12)
+    sensor_temp = seeed_dht.DHT("11", 12)
     counts = 0
     sum = {0, 0}
     while counts < 5:
-        humi, temp = sensor_dht11.read()
+        humi, temp = sensor_temp.read()
         if not humi is None:
-            print('DHT{0}, humidity {1:.1f}%, temperature {2:.1f}*'.format(sensor_dht11.dht_type, humi, temp))
+            print('DHT{0}, humidity {1:.1f}%, temperature {2:.1f}*'.format(sensor_temp.dht_type, humi, temp))
             counts += 1
             sum[0] += humi
             sum[1] += temp
         else:
-            print('DHT{0}, humidity & temperature: {1}'.format(sensor_dht11.dht_type, temp))
+            print('DHT{0}, humidity & temperature: {1}'.format(sensor_temp.dht_type, temp))
     results = {sum[0]/(counts+1), sum[1]/counts}
     print(results) 
 def main():
